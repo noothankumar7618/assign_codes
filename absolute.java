11d0323883286c8682fbea1f1eed32d48d89510e
noothankumar1 <pgnoothankumar1999@gmail.com>
2020-10-16 19:56:53 +0530

    hmm

---
+++ absolute.java
@@ -0,0 +1,17 @@
+package assigncodes;
+
+public class absolute {
+
+	public static void main(String[] args) {
+		double a=-45;
+				double b=56;
+				
+				double absolute1=Math.abs(a);
+				double absolute2=Math.abs(b);
+				
+				System.out.println( absolute1);
+				System.out.println( absolute2);
+
+	}
+
+}
